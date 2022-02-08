d4efbeca221489fcd51bbc1592e67e9c7b1a3755
mscherbela <michael.scherbela@gmail.com>
2022-02-08 21:37:29 +0100

    Fixed static routes

--- backend/app.py
+++ backend/app.py
@@ -17,8 +17,8 @@ def angle_to_heading(angle):
 
 
 @app.route("/")
-@app.route("/static")
-@app.route("/static/")
+@app.route("static")
+@app.route("static/")
 def static_index():
     return redirect('static/index.html')
 
